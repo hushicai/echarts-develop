0922b1c48ffce1fb725d51bacb243b86cd6e6e2d
hushicai <bluthcy@gmail.com>
2015-06-25 20:04:49 +0800

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,45 @@
 
 var path = require('path');
 
+var Alias = {
+    ECHARTS: path.resolve(__dirname, './submodule/ecomfe/echarts/src'),
+    ZRENDER: path.join(__dirname, './submodule/ecomfe/zrender/src')
+};
+
 module.exports = {
-    entry: './asset/test',
+    debug: true,
+    entry: './webpack/entry.js',
     output: {
-        path: path.join(__dirname, 'build/webpack'),
-        filename: '[name].bundle.js'
+        path: path.join(__dirname, 'webpack'),
+        filename: 'build.js'
     },
     resolve: {
         alias: {
-            zrender: './submodule/zrender/src',
-            echarts: './submodule/echarts/src'
+            echarts: Alias.ECHARTS,
+            zrender: Alias.ZRENDER
         }
-    }
+    },
+    plugins: [
+        {
+            apply: function (compiler) {
+                compiler.resolvers.normal.plugin('module', function (request, callback) {
+                    var moduleName = request.request;
+                    console.log(moduleName);
+                    if (moduleName === 'echarts'
+                        || moduleName === 'zrender'
+                    ) {
+                        var obj = {
+                            path: Alias[moduleName.toUpperCase()] + '/' + moduleName + '.js',
+                            file: true,
+                            resolved: true
+                        };
+                        callback(null, obj);
+                    }
+                    else {
+                        callback();
+                    }
+                });
+            }
+        }
+    ]
 };
---
+++ webpack/entry.js
@@ -0,0 +1,42 @@
+/**
+ * @file test
+ * @author hushicai(bluthcy@gmail.com)
+ */
+
+(function (global) {
+    var echarts = require('echarts');
+
+    require('echarts/chart/line');
+
+    var el = document.getElementById('test');
+    var ec = echarts.init(el);
+
+    ec.setOption({
+        title: {
+            text: 'test'
+        },
+        xAxis: [
+            {
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                axisLabel: {
+                    formatter: '{value} °C'
+                }
+            }
+        ],
+        series: [
+            {
+                name: 'test',
+                type: 'line',
+                data: [1, 2, 3]
+            }
+        ]
+    });
+})(this);
+
